13707717db93ae4acbe35dba6fa1c964bb895896
George Antonious <george@antonious.ca>
2018-03-07 10:18:05 -0700

    Add extraness

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ $(function() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - $('.fixed-top').outerHeight() - 20
           }, 800);
           return false;
         }
       }
     });
   });
+
+
+
+$(document).ready(function() {
+  $('.content').css('margin-top', $('.fixed-top').outerHeight());
+});
+
+$(window).resize(function() {
+  $('.content').css('margin-top', $('.fixed-top').outerHeight());
+});
